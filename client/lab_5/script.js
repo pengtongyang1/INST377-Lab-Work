04e13f91e0c1019f48ff7d3a137f139f29366ec0
pengtongyang1 <89853890+pengtongyang1@users.noreply.github.com>
2021-10-01 13:11:32 -0400

    lab 5
    lab 5

---
+++ client/lab_5/script.js
@@ -0,0 +1,5 @@
+res.send(Buffer.from('whoop'))
+res.send({ some: 'json' })
+res.send('<p>http//localhost:3000</p>')
+res.status(404).send('Sorry, we cannot find that!')
+res.status(500).send({ error: 'something blew up' })
