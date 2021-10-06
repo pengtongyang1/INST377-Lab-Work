7e1a2d4a282d3d0af91a921097d6ca1bb1b9df54
pengtongyang1 <89853890+pengtongyang1@users.noreply.github.com>
2021-10-05 21:18:55 -0400

    tetris_update
    tetris_update

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -83,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
       moveDown()
     }
   }
-  document.addEventListener('keyup', control)
+  document.addEventListener('keydown', control)
 
   function moveDown () {
     undraw()
@@ -197,8 +197,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
       scoreDisplay.innerHTML = 'end'
       clearInterval(timerId)
-    }
   }
+}
 
 
 
